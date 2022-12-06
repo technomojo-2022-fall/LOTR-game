9f3900b86d82d4e4601d59f80176f5528f681160
Mark Nair <marknair@me.com>
2022-12-06 17:27:05 +0000

    Adding importing and exporting

--- script.js
+++ script.js
@@ -1,75 +1,26 @@
-const hero = {
-	elementId: "hero",
-	name: "Wizard",
-	avatar: "images/wizard.png",
-	health: "60",
-	diceCount: 3
+import {characterData} from "./data.js"
+import Character from "./Character.js"
+
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
+
+function display() {
+	document.getElementById("hero").innerHTML = wizard.getCharacterHtml()
+	document.getElementById("monster").innerHTML = orc.getCharacterHtml()
 }
-
-const monster = {
-	elementId: "monster",
-	name: "Orc",
-	avatar: "images/orc.png",
-	health: "10",
-	diceCount: 1
-}
-
-function Character(data) {
-	Object.assign(this, data)
-	// this.elementId = data.elementId
-	// this.name = data.name
-	// this.avatar = data.avatar
-	// this.health = data.health
-	// this.diceCount = data.diceCount
-	
-	this.getCharacterHtml = function() {
-		const {elementId, name, avatar, health, diceCount} = this
-		
-		const diceHTML = getDiceHtml(diceCount)
-		
-		
-		document.getElementById(elementId).innerHTML = `
-			<div class="character-card">
-				<h4 class="name"> ${name} </h4>
-				<img class="avatar" src="${avatar}">
-				<p class="health">health: <b> ${health} </b></p>
-				<div class="dice-container">${diceHTML}</div>
-			</div>
-		`
-	}
-}
-
-function getDiceRollArray(diceCount) {
-	const newDiceRolls = []
-	for (let i = 0; i < diceCount; i++) {
-		newDiceRolls.push(Math.floor(Math.random() * 6) + 1)
-	}
-	return newDiceRolls
-}
-
-function getDiceHtml(diceCount) {
-	return getDiceRollArray(diceCount).map(function(number) {
-		return `<div class="dice">${number}</div>`
-	}).join("")
-}
-
-const wizard = new Character(hero)
-const orc = new Character(monster)
-wizard.getCharacterHtml()
-orc.getCharacterHtml()
-
+display()
 getDiceRollArray(3)
 
 
+// 1. Create a file called `utils.js`.
+// 2. Cut and paste the getDiceRollArray() function into it.
+// 3. Import back it into script.js as a default or named export.
+// 4. Create a file called `Character.js`.
+// 5. Cut and paste the Character constructor function into it.
+// 6. Import back it into script.js as a default or named export.
+// 7. You will hit a problem! Solve the problem by using the browser console for hints.
 
 
-// 4. Create two new instances of `Character`. One for a hero, 
-//    called `wizard`, and one for a monster, called `orc`. 
-//    Render both of them on the page.
-// 5. Delete both the old `renderCharacter` function and the two 
-//    lines of code at the bottom of the page which invoke that 
-//    function.
-// 
 
 
 
---
+++ utils.js
@@ -0,0 +1,15 @@
+
+
+function getDiceRollArray(diceCount) {
+	return new Array(diceCount).fill(0).map(function() {
+		return Math.floor(Math.random()*6) +1
+	})
+	
+	// const newDiceRolls = []
+	// for (let i = 0; i < diceCount; i++) {
+	// 	newDiceRolls.push(Math.floor(Math.random() * 6) + 1)
+	// }
+	// return newDiceRolls
+}
+
+export {getDiceRollArray}
